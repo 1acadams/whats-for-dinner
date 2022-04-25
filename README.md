34518d6f2e4603a1d62ef1212b876ad5c37d85de
Allison Adams <allisonclareadams@gmail.com>
2022-04-25 13:37:30 -0400

    added tests and edited README

---
+++ README.md
@@ -0,0 +1,2 @@
+What's For Dinner?
+a web application for people who cook
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,24 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get home_index_url
     assert_response :success
   end
+
+  test "get protein input" do
+    assert input_protein
+  end
+
+  test "get veg input" do
+    assert input_veg
+  end
+
+  test "get aromatics input" do
+    assert input_aromatics
+  end
+
+  test "get oils input" do
+    assert input_oils
+  end
+
+  test "get starch input" do
+    assert input_starch
+  end
 end
